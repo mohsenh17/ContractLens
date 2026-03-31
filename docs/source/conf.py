6f9968f47fd3c9df2568e193202a427808b3443e
mohsenh17 <mohsen.hosseini1272@gmail.com>
2026-03-31 09:44:15 -0400

    first doc

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ContractLens'
+copyright = '2026, Mohsenh17'
+author = 'Mohsenh17'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../..'))
